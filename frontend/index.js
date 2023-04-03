52e91bdebbd65fd2c6116ec089df81345cae1417
yaraschmidt <schmidtyara2709@gmail.com>
2023-04-03 15:53:28 +0000

    insert switch case users in index.js for templates

--- frontend/index.js
+++ frontend/index.js
@@ -9,4 +9,36 @@ async function func(){
     const pageSplit = location.hash.slice(1).split("/");
     const contentDiv = document.getElementById("content");
     contentDiv.innerHTML = "";
+
+    switch (pageSplit[0]) {
+
+        case "users":
+            await fetch("./templates/users.html")
+                .then(resp => resp.text())
+                .then(text => {HTML_TEMPLATE = text});
+
+
+            const userListDiv = document.createElement("div");
+            userListDiv.classList.add("list")
+
+            fetch(backendAPI + "/user")
+                .then(user => user.json())
+                .then(arr => arr.forEach(user => {
+                    let html = HTML_TEMPLATE;
+
+                    html = html.replace(/%NAME%/g, user["name"]);
+                    html = html.replace(/%AGE%/g, user["age"]);
+                    html = html.replace(/%COLOR%/g, user["color"]);
+                    html = html.replace(/%USERID%/g, user["_id"]);
+                    userListDiv.innerHTML += html;
+                }));
+
+            const createUserButton = document.createElement("button");
+            createUserButton.onclick = () => route('user');
+            createUserButton.innerText = "Create User"
+            contentDiv.appendChild(userListDiv);
+            contentDiv.appendChild(createUserButton);
+
+        break;
+    }
 };
