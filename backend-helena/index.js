e6755d4f56246a30efdc5a587515c2fce06532b2
Bhaily <helena.bitsche@gmx.at>
2023-04-07 18:26:45 +0200

    update index.js in backend-helena

--- backend-helena/index.js
+++ backend-helena/index.js
@@ -17,6 +17,56 @@ const baseUrl = "http://localhost:3000";
 app.use(express.json());
 app.use(cors());
 
+app.get("/user", async function(_, res){
+    const arr = await userDocuments.find({}).toArray()
+    res.json(arr.map(processUser))
+});
+
+app.get("/user/:id", async function(req, res) {
+    const {id,} = req.params;
+    if(!id) return res.sendStatus(400);
+
+    const user = await userDocuments.findOne({_id : new ObjectId(id)});
+    res.json(processUser(user));
+});
+
+app.post("/user", async function(req, res){
+    const {name, age, color} = req.body;
+    if(!name || !age || !color) return res.sendStatus(400);
+    const user = {
+        name : name,
+        age : age,
+        color : color
+    }
+    const response = await userDocuments.insertOne(user);
+    user._id = response.insertedId;
+    res.json(processUser(user));
+});
+
+app.put("/user", async function(req, res) {
+    const {_id, name, age, color} = req.body;
+    if(!_id || !name || !age || !color) return res.sendStatus(400);
+    const user = {
+        _id : new ObjectId(_id),
+        name : name,
+        age : age,
+        color : color
+    }
+    const response = await userDocuments.updateOne({_id :  user._id}, {$set : user});
+    res.json(processUser(user));
+});
+
+app.delete("/user/:id", async function(req, res) {
+    const {id,} = req.params;
+    if(!id) return res.sendStatus(400);
+    const response = await userDocuments.deleteOne({_id : new ObjectId(id)});
+    if(response.acknowledged && response.deletedCount == 1){
+        res.sendStatus(200);
+    }else{
+        res.sendStatus(404);
+    }
+});
+
 app.listen(3000, "0.0.0.0", function() {
     console.log("Server gestartet!");
 });
